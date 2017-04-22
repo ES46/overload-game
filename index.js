0eedab8a8660a8055a61824f2b22a4cdc9d25003
lrobinsonco <lrobinsonco@gmail.com>
2017-04-22 16:16:29 -0600

    added page gets

--- index.js
+++ index.js
@@ -12,14 +12,21 @@ app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('splash')
 })
+
+app.get('/login', (req, res) => {
+  // linkQuery.getPage(req.params.id)
+  // .then(data => {
+  res.render('index');
+});
 
 app.get('/game', (req, res) => {
   // linkQuery.getPage(req.params.id)
   // .then(data => {
   res.render('game');
 });
+
 
 app.listen(port, () => {
   console.log(`listening on ${port}`);
