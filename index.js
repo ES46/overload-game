5d37a7263c8f51343669f60531a88ea83fcc6c62
Nick McKendry <nickmckendry@gmail.com>
2017-04-23 14:40:57 -0600

    style changes 💥

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const linkQuery = require('./db/linkQuery')
 const bodyParser = require('body-parser')
 const app = express()
 const pg = require('./db/knex')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3010
 
 app.set('view engine', 'hbs')
 
