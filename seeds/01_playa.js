70a55a12d15d6547a6bc93a036c5adbc9b061934
Lena Ripple <ripplelena@gmail.com>
2017-04-25 13:26:08 -0600

    splash updates

--- seeds/01_playa.js
+++ seeds/01_playa.js
@@ -5,26 +5,26 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('player').insert([
-        {
-          id:1,
-          playername: 'nick',
-          score: 2
-        },
-        {
-          id:2,
-          playername: 'lee',
-          score: 2
-        },
-        {
-          id:3,
-          playername: 'derek',
-          score: 2
-        },
-        {
-          id:4,
-          playername: 'Lena',
-          score: 2
-        }
+      //   {
+      //     id:1,
+      //     playername: 'nick',
+      //     score: 2
+      //   },
+      //   {
+      //     id:2,
+      //     playername: 'lee',
+      //     score: 2
+      //   },
+      //   {
+      //     id:3,
+      //     playername: 'derek',
+      //     score: 2
+      //   },
+      //   {
+      //     id:4,
+      //     playername: 'Lena',
+      //     score: 2
+      //   }
       ]);
     });
 };
